44c0b70e595fa138d71709db471b7313d9cdb31f
MrGJ <haidenpburns20@gmail.com>
2020-08-19 11:21:15 +1000

    Animation Script Done
    It is done, now to duplicate the same lines over and over again :100: :100: :100: :100: :100: :100: :100: :100: :100: :100:

--- Assets/Scripts/AnimationSwapper.cs
+++ Assets/Scripts/AnimationSwapper.cs
@@ -9,6 +9,8 @@ public class AnimationSwapper : MonoBehaviour
     public Animator luccile;
     public GameObject textBox;
     bool wavyArmsBool;
+    bool openWavyArmsBool;
+    bool shrugBool;
 
     // Start is called before the first frame update
     void Start()
@@ -30,17 +32,68 @@ public class AnimationSwapper : MonoBehaviour
             luccile.SetBool("Cutscene_Start", false);
         }
 
-        if (animationTracker == 1)
+        if (textBox.activeSelf == true)
         {
-            if (wavyArmsBool == false)
+            if (animationTracker == 1)
             {
-                StartCoroutine(WavyArms());
+                if (shrugBool == false)
+                {
+                    StartCoroutine(Shrug());
+                }
             }
-        }
-        else if (animationTracker == 2)
-        {
-            StopAllCoroutines();
-            luccile.SetBool("Open_Mouth_Swap_Bool", true);
+            else if (animationTracker == 2)
+            {
+                ResetAnimations();
+                OpenMouth();
+            }
+            else if (animationTracker == 3)
+            {
+                ResetAnimations();
+            }
+            else if (animationTracker == 4 || animationTracker == 5)
+            {
+                if (openWavyArmsBool == false)
+                {
+                    StartCoroutine(OpenWavyArms());
+                }
+            }
+            else if (animationTracker == 6)
+            {
+                ResetAnimations();
+            }
+            else if (animationTracker == 7)
+            {
+                if (shrugBool == false)
+                {
+                    StartCoroutine(Shrug());
+                }
+            }
+            else if (animationTracker == 8)
+            {
+                ResetAnimations();
+            }
+            else if (animationTracker == 11)
+            {
+                if (shrugBool == false)
+                {
+                    StartCoroutine(Shrug());
+                }
+            }
+            else if (animationTracker == 12)
+            {
+                OpenMouth();
+            }
+            else if (animationTracker == 13)
+            {
+                if (wavyArmsBool == false)
+                {
+                    StartCoroutine(WavyArms());
+                }
+            }
+            else if (animationTracker == 14)
+            {
+                ResetAnimations();
+            }
         }
     }
 
@@ -51,4 +104,38 @@ public class AnimationSwapper : MonoBehaviour
         luccile.SetBool("Wavy_Arms_Bool", false);
         wavyArmsBool = true;
     }
+
+    IEnumerator OpenWavyArms()
+    {
+        luccile.SetBool("Open_Wavy_Arms_Bool", true);
+        yield return new WaitForSeconds(5f);
+        luccile.SetBool("Open_Wavy_Arms_Bool", false);
+        openWavyArmsBool = true;
+    }
+
+    IEnumerator Shrug()
+    {
+        luccile.SetBool("Shrug_Bool", true);
+        yield return new WaitForSeconds(1f);
+        luccile.SetBool("Shrug_Bool", false);
+        shrugBool = true;
+
+    }
+
+    private void OpenMouth()
+    {
+        luccile.SetBool("Open_Mouth_Swap_Bool", true);
+    }
+
+    private void ResetAnimations()
+    {
+        luccile.SetBool("Open_Mouth_Swap_Bool", false);
+        luccile.SetBool("Wavy_Arms_Bool", false);
+        luccile.SetBool("Open_Wavy_Arms_Bool", false);
+        luccile.SetBool("Shrug_Bool", false);
+        openWavyArmsBool = false;
+        wavyArmsBool = false;
+        shrugBool = false;
+    }
+
 }
