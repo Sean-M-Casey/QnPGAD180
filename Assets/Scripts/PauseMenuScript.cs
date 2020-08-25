2f3695bf67ff17c5d76e1f80003f23af4624d7b4
Archie-Paxton <67458177+Archie-Paxton@users.noreply.github.com>
2020-08-25 14:37:29 +1000

    Pause Menu & 2 Assets added
    Added the pause menu to the game and it has its functions.
    
    The Tv for the LivingRoom and Mexican Statue for Diningroom have been added and set up
    
    gang gang

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    //First Varible is for the player controls script
+    //Second Varible is for Pausemenu Sprite
+    public PlayerControls controlScript;
+    public Image pauseMenu;
+    bool toggle = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.gameObject.SetActive(false);
+    }
+
+    // Escape if the default key for activating the pause menu 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            toggle = !toggle;
+        }
+        //this If statement is setting the PauseMenu active, activating the mouse & disabling the player movement  
+        if (toggle == false)
+        {
+            pauseMenu.gameObject.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            controlScript.enabled = true;
+        }
+        else if (toggle == true)
+        {
+            pauseMenu.gameObject.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
+            controlScript.enabled = false;
+        }
+    }
+
+    public void ResumeButton()
+    {
+        toggle = !toggle;
+    }
+    //this is for setting which scene is going to be loaded
+    public void QuitButton()
+    {
+        SceneManager.LoadScene("Intro_Splash_Screen");
+    }
+}
